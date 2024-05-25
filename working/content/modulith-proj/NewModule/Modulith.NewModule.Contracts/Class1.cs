cdc689323582ce0acb451097840d26a41e2cd5da
davidchaparro <davidacm7@outlook.com>
2024-05-25 09:45:29 -0400

    Closes #28 Add test script. Removes unused templates. These templates were moved to the single modulith/folder

--- working/content/modulith-proj/NewModule/Modulith.NewModule.Contracts/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Modulith.NewModule.Contracts;
-
-public class Class1
-{
-
-}
--- working/content/modulith-proj/NewModule/Modulith.NewModule/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Modulith.NewModule.Tests")]
-namespace Modulith.NewModule;
-
-public class AssemblyInfo { }
-
