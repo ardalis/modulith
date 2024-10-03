db52d7ebd9af68984e688ed175a8cfa04102b6b7
Kevin Lloyd <kevin@kevinlloyd.net>
2024-10-03 16:24:40 -0500

    Matching variable name on interface

--- working/content/modulith/NewModule/Modulith.NewModule/NewModuleModuleServiceRegistrar.cs
+++ working/content/modulith/NewModule/Modulith.NewModule/NewModuleModuleServiceRegistrar.cs
@@ -10,7 +10,7 @@ namespace Modulith.NewModule;
 public class NewModuleModuleServiceRegistrar : IRegisterModuleServices
 {
   public static IServiceCollection ConfigureServices(IServiceCollection services,
-    IConfiguration config)
+    IConfiguration configuration)
   {
     services.AddMediatR(
       c => c.RegisterServicesFromAssemblies(typeof(AssemblyInfo).Assembly));
