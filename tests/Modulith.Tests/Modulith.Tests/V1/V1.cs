d37ef66ba06aa332953639d045d0c075a3f898b3
david-acm <davidacm7@outlook.com>
2024-10-04 14:05:51 -0400

    Add previous version test
    Bump refactored templates to 2.0

---
+++ tests/Modulith.Tests/Modulith.Tests/V1/V1.cs
@@ -0,0 +1,50 @@
+using Modulith.Tests.Extensions;
+using Xunit.Abstractions;
+
+namespace Modulith.Tests.Solution;
+
+public class V1(ITestOutputHelper output) : TestBase(output)
+{
+  [Fact]
+  public async Task Solution()
+  {
+    // dotnet new modulith -n eShop --with-module Payments
+    await Engine.Verify(options =>
+      options.WithArgs([
+          "-n", "eShop",
+          "--with-module", "Payments",
+          "-o", "eShop"
+        ])
+        .DisableDiffTool()
+        .DeletingVerifyCompilationFiles());
+  }
+
+  [Fact]
+  public async Task WithBasicModule()
+  {
+    // var outputDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+    var solutionOptions = await Engine.TryVerify(o =>
+      o.WithArgs([
+          "-n", "eShop",
+          "--with-module", "Payments",
+          "-o", "eShop"
+        ])
+        .KeepInstantiationInSnapshot()
+        .DeletingVerifyCompilationFiles()
+        .DeletingReceivingDirectory()
+        // .WithOutputDirectory(outputDirectory)
+    );
+
+    // dotnet new modulith --add basic-module --with-name Shipments --to eShop
+    await Engine.Verify(
+      o => o.WithArgs([
+          "--add", "basic-module",
+          "--with-name", "Shipments",
+          "--to", "eShop",
+          "-o", "eShop",
+        ])
+        .KeepInstantiationInSnapshot()
+        .DisableDiffTool(false)
+        .WithOutputDirectory(solutionOptions.OutputDirectory!));
+  }
+}
